4f07176231863af2610f1fdaf54d011f9308c123
jerry <rabbitron295@gmail.com>
2023-10-23 14:45:58 +0530

    hello

---
+++ anbot.py
@@ -0,0 +1,60 @@
+import speech_recognition as sr
+import os
+from gtts import gTTS
+import time
+import requests 
+import datetime
+recognizer = sr.Recognizer()
+url = 'http://api.brainshop.ai/get?bid=178546&key=gCppj0KnUpICcFI1&uid=Eldhose&msg='
+def send_message(input_text):
+    response = requests.get(url + input_text)
+    data = response.json()
+    return data['cnt']
+def audio_gen(text):
+        #audio = elevenlabs.generate(
+         #       text,
+          #      voice = "Glinda"
+           #      )
+        #elevenlabs.save(audio, "output.mp3")
+        tts = gTTS(text)
+        tts.save("output.mp3")
+        os.system("termux-media-player play output.mp3 2&> /dev/null")
+def times():
+    current_time = datetime.datetime.now()
+    return current_time.strftime('%Y-%m-%d %H:%M:%S')
+def rec(val):
+    os.system(f"termux-microphone-record -e aac -l {val} -f ./audio.aac");
+    time.sleep(val+1)
+    os.system("ffmpeg -i audio.aac audio.wav")
+    audio_file = "audio.wav"
+    with sr.AudioFile(audio_file) as source:
+        audio = recognizer.record(source)
+    try:
+        text = recognizer.recognize_google(audio).lower()
+        print("You said: " + text)
+    except sr.UnknownValueError:
+        text = ""
+        print("Google Web Speech API could not understand audio.")
+    except sr.RequestError as e:
+        text = ""
+        print("Could not request results from Google Web Speech API; {0}".format(e))
+    os.system("rm audio.aac audio.wav")
+    return text
+while True:
+    text = rec(3)
+    print(text)
+    if "remmacs" in text or "remix" in text or "remax" in text:
+        audio_gen("hey there")
+        while True:
+            text = rec(5)
+            if "time" in text:
+                text = times()
+            if "goodbye" in text:
+                audio_gen("goodbye")
+                break
+            else:
+                text = text.replace(" ", "%20")
+                text = send_message(text)
+            print(text)
+            audio_gen(text)
+
